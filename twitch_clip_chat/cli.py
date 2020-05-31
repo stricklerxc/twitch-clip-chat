869d77548db9773eb344bca66da08459691b8cd0
stricklerxc <29963548+stricklerxc@users.noreply.github.com>
2020-05-31 12:59:47 -0400

    create entrypoint function

--- twitch_clip_chat/cli.py
+++ twitch_clip_chat/cli.py
@@ -3,37 +3,34 @@
 import requests
 import sys
 
-import pprint
+from .config import Config
+from .parser import setup_parser
+from .twitch import make_request, grab_comments
 
-from twitch_clip_chat.config import Config
-from twitch_clip_chat.parser import setup_parser
-from twitch_clip_chat.twitch import make_request, grab_comments
+def main():
+    parser = setup_parser()
+    options = parser.parse_args()
 
-pp = pprint.PrettyPrinter(indent=2)
+    config = Config()
 
-parser = setup_parser()
-options = parser.parse_args()
+    # Get Clip Metadata
+    url = f"https://api.twitch.tv/kraken/clips/{options.video_id}"
+    # url = f"https://api.twitch.tv/helix/clips?id={options.video_id}"
+    raw_resp = make_request(config, url)
+    resp = raw_resp.json()
 
-config = Config()
+    if raw_resp.status_code == 404:
+        print('Twitch clip has been deleted')
+        sys.exit(1)
 
-# Get Clip Metadata
-url = f"https://api.twitch.tv/kraken/clips/{options.video_id}"
-# url = f"https://api.twitch.tv/helix/clips?id={options.video_id}"
-raw_resp = make_request(config, url)
-resp = raw_resp.json()
+    vod = resp.get('vod')
 
-if raw_resp.status_code == 404:
-    print('Twitch clip has been deleted')
-    sys.exit(1)
+    if vod:
+        _id = vod['id']
+        offset = vod['offset']
+        duration = resp['duration']
 
-vod = resp.get('vod')
-
-if vod:
-    _id = vod['id']
-    offset = vod['offset']
-    duration = resp['duration']
-
-    # Get Comments
-    grab_comments(_id, offset, duration, config, options)
-else:
-    print('No vod listed, no comments to grab')
+        # Get Comments
+        grab_comments(_id, offset, duration, config, options)
+    else:
+        print('No vod listed, no comments to grab')
