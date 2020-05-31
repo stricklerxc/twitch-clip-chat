3a8d8cae7116913f8601c377a0cc2ab69b0cf040
stricklerxc <29963548+stricklerxc@users.noreply.github.com>
2020-05-31 14:09:06 -0400

    add profile support

--- twitch_clip_chat/config.py
+++ twitch_clip_chat/config.py
@@ -9,30 +9,71 @@ class Config():
         self.config_file = ConfigParser()
         self.config_file.read(self.creds_file)
 
-        self.profile = 'default'
-        self.credentials = self.config_file[self.profile]
-        self.client_id = self.credentials.get('TWITCH_CLIENT_ID')
-        self.client_secret = self.credentials.get('TWITCH_CLIENT_SECRET')
+        self._profile = None
+        self.profile = kwargs['profile'] if kwargs.get('profile') else 'default'
         self.kwargs = kwargs
 
     @property
     def bearer_token(self):
-        return self.credentials.get('TWITCH_BEARER_TOKEN')
+        return self.config_file[self.profile].get('TWITCH_BEARER_TOKEN')
 
     @bearer_token.setter
     def bearer_token(self, value):
-        self.credentials['TWITCH_BEARER_TOKEN'] = value
+        self.config_file[self.profile]['TWITCH_BEARER_TOKEN'] = value
 
         with open(self.creds_file, 'w') as file_handler:
             self.config_file.write(file_handler)
 
     @property
     def bearer_token_expiration(self):
-        return self.credentials.get('TWITCH_BEARER_TOKEN_EXPIRATION')
+        return self.config_file[self.profile].get('TWITCH_BEARER_TOKEN_EXPIRATION')
 
     @bearer_token_expiration.setter
     def bearer_token_expiration(self, value):
-        self.credentials['TWITCH_BEARER_TOKEN_EXPIRATION'] = value
+        self.config_file[self.profile]['TWITCH_BEARER_TOKEN_EXPIRATION'] = value
 
         with open(self.creds_file, 'w') as file_handler:
             self.config_file.write(file_handler)
+
+    @property
+    def client_id(self):
+        return self.config_file[self.profile].get('TWITCH_CLIENT_ID')
+
+    @client_id.setter
+    def client_id(self, value):
+        self.config_file[self.profile]['TWITCH_CLIENT_ID'] = value
+
+        with open(self.creds_file, 'w') as file_handler:
+            self.config_file.write(file_handler)
+
+    @property
+    def client_secret(self):
+        return self.config_file[self.profile].get('TWITCH_CLIENT_SECRET')
+
+    @client_secret.setter
+    def client_secret(self, value):
+        self.config_file[self.profile]['TWITCH_CLIENT_SECRET'] = value
+
+        with open(self.creds_file, 'w') as file_handler:
+            self.config_file.write(file_handler)
+
+    @property
+    def profile(self):
+        return self._profile
+
+    @profile.setter
+    def profile(self, value):
+        # Set profile if profile already defined in config file
+        if self.config_file.has_section(value):
+            self._profile = value
+        # If profile does not exist in config file
+        else:
+            # Add profile to config object
+            self.config_file.add_section(value)
+
+            # Write config object to config file
+            with open(self.creds_file, 'w') as file_handler:
+                self.config_file.write(file_handler)
+
+            # Set profile
+            self._profile = value
--- twitch_clip_chat/parser.py
+++ twitch_clip_chat/parser.py
@@ -1,10 +1,17 @@
 import argparse
 
 def setup_parser():
-    parser = argparse.ArgumentParser()
+    parent = argparse.ArgumentParser(description="Twitch Clip Chat - Scrape chat from Twitch Clips")
 
-    # parser.add_argument('-c', '--client-id', dest='client_id' ,metavar='', help='Client ID from https://dev.twitch.tv/console/apps')
-    # parser.add_argument('-s', '--client-secret', dest='client_secret', metavar='', help='Client secret from https://dev.twitch.tv/console/apps')
-    parser.add_argument('video_id', metavar='ID', help='Video Id/Slug for the Twitch Clip')
+    commands = parent.add_subparsers(title='commands', dest='command')
+    get = commands.add_parser('get', help='Scrapes the chat from the given Twitch clip')
+    get.add_argument('video_id', metavar='ID', help='Video Id/Slug for the Twitch Clip')
+    get.add_argument('-p', '--profile', dest='profile', metavar='', help='Profile to store the Twitch credentials in')
 
-    return parser
+
+    configure = commands.add_parser('configure', help='Configure Twitch Credentials')
+    configure.add_argument('-c', '--client-id', dest='client_id' ,metavar='', help='Client ID from https://dev.twitch.tv/console/apps')
+    configure.add_argument('-s', '--client-secret', dest='client_secret', metavar='', help='Client secret from https://dev.twitch.tv/console/apps')
+    configure.add_argument('-p', '--profile', dest='profile', metavar='', help='Profile to store the Twitch credentials in')
+
+    return parent
