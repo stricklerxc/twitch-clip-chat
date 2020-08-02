feb069f24b0da9bee89bad22634515ad2a7bec92
stricklerxc <29963548+stricklerxc@users.noreply.github.com>
2020-08-02 09:06:01 -0400

    add headers to csv output

--- twitch_clip_chat/twitch.py
+++ twitch_clip_chat/twitch.py
@@ -45,5 +45,7 @@ def grab_comments(_id, offset, duration, config, options):
         resp = make_request(config, url).json()
 
     with open(f'{options.video_id}.csv', 'w', newline='') as file_handler:
+        headers = ['Timestamp', 'Username', 'Message']
         csv_writer = writer(file_handler)
+        csv_writer.writerow(headers)
         csv_writer.writerows(clip_comments)
